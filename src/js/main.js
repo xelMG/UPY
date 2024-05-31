7178eb3fef57ed2d87b810a62b31c3459f038912
kali <kali@kali.kali>
2024-05-30 20:10:51 -0600

    Initial project structure

---
+++ src/js/main.js
@@ -0,0 +1,1 @@
+document.getElementById('check').addEventListener('click',function(){const password = document.getElementById('password').value;document.getElementById('feedback').textContent =`Password length: ${password.length}`;});
